60cfb3a494665c594ac4fdd678a0da758139d49a
Tsaregorodtsev <sanych99@gmail.com>
2018-03-14 02:46:39 +0600

    Executor in BaseHandler

--- NetCoreCQRS/Handlers/BaseHandler.cs
+++ NetCoreCQRS/Handlers/BaseHandler.cs
@@ -1,6 +1,10 @@
-﻿namespace NetCoreCQRS.Handlers
+﻿using NetCoreDI;
+
+namespace NetCoreCQRS.Handlers
 {
     public class BaseHandler
     {
+        private IExecutor _executor;
+        protected IExecutor Executor => _executor ?? (_executor = AmbientContext.Current.Resolver.ResolveObject<IExecutor>());
     }
 }
