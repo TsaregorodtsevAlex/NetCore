ee21fe9a00fa66c6af72fedad9a8f48304c3abe8
Tsaregorodtsev <sanych99@gmail.com>
2018-04-22 01:09:12 +0600

    NetCoreDomain.csproj

---
+++ NetCoreDomain/Result.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace NetCoreDomain
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public string Error { get; private set; }
+        public bool IsFailure => IsSuccess == false;
+
+        public Result(bool isSuccess, string error)
+        {
+            if (isSuccess && string.IsNullOrEmpty(error) == false)
+            {
+                throw  new InvalidOperationException();
+            }
+
+            if (isSuccess == false && string.IsNullOrEmpty(error))
+            {
+                throw new InvalidOperationException();
+            }
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Ok()
+        {
+            return new Result(true, string.Empty);
+        }
+
+        public static Result Fail(string failMessage)
+        {
+            return new Result(false, failMessage);
+        }
+
+
+    }
+
+    public class Result<T> : Result
+    {
+        private readonly T _value;
+
+        public T Value
+        {
+            get
+            {
+                if (IsFailure)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return _value;
+            }
+        }
+
+        public Result(T value, bool isSuccess, string error) : base(isSuccess, error)
+        {
+            _value = value;
+        }
+    }
+}
---
+++ NetCoreDomain/ValueObject.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace NetCoreDomain
+{
+    public abstract class ValueObject<T> where T : ValueObject<T>
+    {
+        public override bool Equals(object obj)
+        {
+            var valueObject = obj as T;
+
+            if (ReferenceEquals(valueObject, null))
+            {
+                return false;
+            }
+
+            return EqualsCore(valueObject);
+        }
+
+        protected abstract bool EqualsCore(T other);
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        protected abstract int GetHashCodeCore();
+
+        public static bool operator ==(ValueObject<T> first, ValueObject<T> second)
+        {
+            if (ReferenceEquals(first, null) && ReferenceEquals(second, null))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(first, null) || ReferenceEquals(second, null))
+            {
+                return false;
+            }
+
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(ValueObject<T> first, ValueObject<T> second)
+        {
+            return !(first == second);
+        }
+    }
+}
