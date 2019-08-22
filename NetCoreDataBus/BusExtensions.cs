2668dfef23ba9fe3bd176e8e465dcf29bead8b0d
a.caregorodcev <a.caregorodcev@magdv.com>
2019-08-22 15:55:30 +0700

    BusExtensions. Отложенная публикация сообщений

---
+++ NetCoreDataBus/BusExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace NetCoreDataBus
+{
+	public static class BusExtensions
+	{
+		public static async Task<Guid> SendScheduledMessage<TMessage>(this IBus bus, TMessage message, DateTime scheduledTime)
+		{
+			
+			var sendResult =  await bus.ScheduleSend(new Uri($"rabbitmq://{bus.Address.Host}/{typeof(TMessage).Namespace}:{nameof(TMessage)}"), scheduledTime, message);
+			return sendResult.TokenId;
+		}
+
+		public static async Task CancelScheduledMessage(this IBus bus, Guid tokenId)
+		{
+			await bus.CancelScheduledSend(tokenId);
+		}
+	}
+}
