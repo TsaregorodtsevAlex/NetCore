0a4aba31486928278e85efcc09ad859a7f8100ad
Сергей Лебедев <s.lebedev@magdv.com>
2019-10-16 17:48:18 +0700

    NetCoreDataBus.2.2.4.4. Cделал добавление JOB c использованием MassTransit.Scheduling

--- NetCoreDataBus/BusExtensions.cs
+++ NetCoreDataBus/BusExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using MassTransit;
+using MassTransit.Scheduling;
 
 namespace NetCoreDataBus
 {
@@ -8,14 +9,31 @@ namespace NetCoreDataBus
 	{
 		public static async Task<Guid> SendScheduledMessage<TMessage>(this IBus bus, TMessage message, DateTime scheduledTime)
 		{
-			
-			var sendResult =  await bus.ScheduleSend(new Uri($"rabbitmq://{bus.Address.Host}/{typeof(TMessage).Namespace}:{typeof(TMessage).Name}"), scheduledTime, message);
+			var sendResult = await bus.ScheduleSend(new Uri($"rabbitmq://{bus.Address.Host}/{typeof(TMessage).Namespace}:{typeof(TMessage).Name}"), scheduledTime, message);
 			return sendResult.TokenId;
 		}
 
 		public static async Task CancelScheduledMessage(this IBus bus, Guid tokenId)
 		{
 			await bus.CancelScheduledSend(tokenId);
+		}
+
+		public static async Task<ScheduledRecurringMessage> ScheduleRecurringMessage<TMessage, TRecurringSchedule>(this IBus bus, TRecurringSchedule recurringSchedule, TMessage message)
+			where TRecurringSchedule : DefaultRecurringSchedule
+		{
+			var scheduledRecurringMessage = await bus.ScheduleRecurringSend(new Uri($"rabbitmq://{bus.Address.Host}/{typeof(TMessage).Namespace}:{typeof(TMessage).Name}"), recurringSchedule, message);
+
+			return scheduledRecurringMessage;
+		}
+		public static async Task CancelScheduledRecurringMessage<TMessage>(this IBus bus, ScheduledRecurringMessage<TMessage> recurringMessage)
+			where TMessage : class
+		{
+			await bus.CancelScheduledRecurringSend(recurringMessage);
+		}
+		public static async Task CancelScheduledRecurringMessage<TMessage>(this IBus bus, string scheduleId, string scheduleGroup)
+			where TMessage : class
+		{
+			await bus.CancelScheduledRecurringSend(scheduleId, scheduleGroup);
 		}
 	}
 }
