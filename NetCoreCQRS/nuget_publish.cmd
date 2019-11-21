486d82200a90438b75d4834557be8e9db4d833f9
Сергей Лебедев <s.lebedev@magdv.com>
2019-11-21 11:08:02 +0700

    версии библиотек подняты до Version 4.0.0-rc1

--- NetCoreCQRS/nuget_publish.cmd
+++ NetCoreCQRS/nuget_publish.cmd
@@ -1,4 +1,4 @@
 dotnet pack -c Release
-dotnet nuget push bin/Release/NetCoreCQRS.2.2.2.1.nupkg -k oy2ftcuea4qichnsk2yawfx4hkdagzienbq2yp2j3sdwsy -s https://api.nuget.org/v3/index.json
+dotnet nuget push bin/Release/NetCoreCQRS.4.0.0.1-rc1.nupkg -k oy2ftcuea4qichnsk2yawfx4hkdagzienbq2yp2j3sdwsy -s https://api.nuget.org/v3/index.json
 
 pause
--- NetCoreDomain/nuget_publish.cmd
+++ NetCoreDomain/nuget_publish.cmd
@@ -1,4 +1,4 @@
 dotnet pack -c Release
-dotnet nuget push bin/Release/NetCoreDomain.2.0.0.2.nupkg -k oy2ftcuea4qichnsk2yawfx4hkdagzienbq2yp2j3sdwsy -s https://api.nuget.org/v3/index.json
+dotnet nuget push bin/Release/NetCoreDomain.4.0.0-rc1.nupkg -k oy2ftcuea4qichnsk2yawfx4hkdagzienbq2yp2j3sdwsy -s https://api.nuget.org/v3/index.json
 
 pause
