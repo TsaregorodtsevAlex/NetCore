aa718536c021559f063cabf98873a03bf1494be6
Сергей Лебедев <s.lebedev@magdv.com>
2019-11-21 17:28:29 +0700

    NetCoreDataBus - 4.0.0.5-rc2б изменена логика регистрации консюмеров, саг.

--- NetCoreDataBus/DataBusServiceCollectionExtensions.cs
+++ NetCoreDataBus/DataBusServiceCollectionExtensions.cs
@@ -1,144 +1,104 @@
-﻿using System;
-using Automatonymous;
+﻿using Automatonymous;
 using GreenPipes;
 using MassTransit;
 using MassTransit.EntityFrameworkCoreIntegration.Saga;
 using MassTransit.RabbitMqTransport;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace NetCoreDataBus
 {
-    public static class DataBusServiceCollectionExtensions
-    {
-        private static IRabbitMqHost _host;
+	public static class DataBusServiceCollectionExtensions
+	{
+		private static bool _useQuartz;
 
-        private static bool _useQuartz;
-
-
-		public static IServiceCollection AddDataBusConfiguration(this IServiceCollection serviceCollection, IConfiguration configuration)
+		public static IRabbitMqBusFactoryConfigurator AddDataBusConfiguration(this IRabbitMqBusFactoryConfigurator cfg, IServiceCollection services, IConfiguration configuration)
 		{
 			var userName = configuration["RABBITMQ_USERNAME"];
-            var password = configuration["RABBITMQ_PASSWORD"];
-            var hostUrl = configuration["RABBITMQ_HOSTURL"];
-            _useQuartz = string.IsNullOrEmpty(configuration["RABBITMQ_QUARTZ_QUEUE_NAME"]) == false;
-            var quartzQueueName = configuration["RABBITMQ_QUARTZ_QUEUE_NAME"];
+			var password = configuration["RABBITMQ_PASSWORD"];
+			var hostUrl = configuration["RABBITMQ_HOSTURL"];
+			_useQuartz = string.IsNullOrEmpty(configuration["RABBITMQ_QUARTZ_QUEUE_NAME"]) == false;
+			var quartzQueueName = configuration["RABBITMQ_QUARTZ_QUEUE_NAME"];
 
-			var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
-            {
-                _host = cfg.Host(new Uri(hostUrl), h =>
-                {
-                    h.Username(userName);
-                    h.Password(password);
-                });
+			var host = cfg.Host(new Uri(hostUrl), h =>
+			{
+				h.Username("guest");
+				h.Password("guest");
+			});
 
-                if (_useQuartz)
-                {
-	                cfg.UseMessageScheduler(new Uri($"rabbitmq://{_host.Settings.Host}/{quartzQueueName}"));
+			if (_useQuartz)
+			{
+				cfg.UseMessageScheduler(new Uri($"rabbitmq://{host.Settings.Host}/{quartzQueueName}"));
+			}
+
+			services.AddSingleton<IPublishEndpoint>(provider => provider.GetRequiredService<IBusControl>());
+			services.AddSingleton<ISendEndpointProvider>(provider => provider.GetRequiredService<IBusControl>());
+			services.AddSingleton<IBus>(provider => provider.GetRequiredService<IBusControl>());
+
+			return cfg;
+		}
+
+		public static IRabbitMqBusFactoryConfigurator RegisterConsumerWithRetry<TConsumer, TContract>(this IRabbitMqBusFactoryConfigurator cfg, IServiceProvider provider, int retryCount, int intervalMin, int concurrencyLimit = 0)
+			where TConsumer : class, IConsumer
+			where TContract : class
+		{
+			var queueName = $"{typeof(TConsumer).FullName}_{typeof(TContract)}";
+
+			cfg.ReceiveEndpoint(queueName, e =>
+			{
+				if (_useQuartz)
+				{
+					e.UseScheduledRedelivery(r => r.Intervals(
+						TimeSpan.FromMinutes(5),
+						TimeSpan.FromMinutes(15),
+						TimeSpan.FromMinutes(30),
+						TimeSpan.FromMinutes(60),
+						TimeSpan.FromMinutes(120),
+						TimeSpan.FromMinutes(240)));
 				}
-            });
-            serviceCollection.AddSingleton<IPublishEndpoint>(bus);
-            serviceCollection.AddSingleton<ISendEndpointProvider>(bus);
-            serviceCollection.AddSingleton<IBus>(bus);
 
-            bus.Start();
+				e.UseMessageRetry(configurator =>
+				{
+					configurator.Interval(retryCount, TimeSpan.FromMinutes(intervalMin));
+				});
 
-            return serviceCollection;
-        }
+				e.ConfigureConsumer<TConsumer>(provider, ccfg =>
+				{
+					if (concurrencyLimit > 0)
+					{
+						ccfg.UseConcurrencyLimit(concurrencyLimit);
+						ccfg.UseConcurrentMessageLimit(concurrencyLimit);
+					}
+					else
+					{
+						ccfg.Message<TContract>(m => m.UsePartitioner(1, context => context.MessageId.Value));
+					}
+				});
 
-        public static IServiceCollection RegisterConsumerWithRetry<TConsumer, TContract>(this IServiceCollection serviceCollection, int retryCount, int intervalMin, int concurrencyLimit = 0)
-            where TConsumer : class, IConsumer, new() where TContract: class
-        {
-            var queueName = $"{typeof(TConsumer).FullName}_{typeof(TContract)}";
+			});
 
-            _host.ConnectReceiveEndpoint(queueName, cfg =>
-                {
-                    cfg.Consumer<TConsumer>(x =>
-                        {
-	                        if (_useQuartz)
-	                        {
-		                        x.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5),
-			                        TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(30), TimeSpan.FromMinutes(60),
-			                        TimeSpan.FromMinutes(120), TimeSpan.FromMinutes(240)));
-	                        }
+			cfg.AutoDelete = false;
+			cfg.Durable = true;
 
-	                        x.UseMessageRetry(configurator =>
-                            {
-                                configurator.Interval(retryCount, TimeSpan.FromMinutes(intervalMin));
-                            });
+			return cfg;
+		}
 
-	                        if(concurrencyLimit > 0)
-                            {
-                                x.UseConcurrencyLimit(concurrencyLimit);
-                                x.UseConcurrentMessageLimit(concurrencyLimit);
-                            }
-                            else
-                            {
-	                            x.Message<TContract>(m => m.UsePartitioner(1, context => context.MessageId.Value));
-							}
-                        });
+		public static IRabbitMqBusFactoryConfigurator RegisterSaga<TStateMachineInstance>(this IRabbitMqBusFactoryConfigurator cfg,
+			MassTransitStateMachine<TStateMachineInstance> sagaStateMachine,
+			EntityFrameworkSagaRepository<TStateMachineInstance> sagaRepository)
+			where TStateMachineInstance : class, SagaStateMachineInstance, new()
+		{
+			var queueName = $"{typeof(TStateMachineInstance).FullName}";
 
-                    cfg.AutoDelete = false;
-                    cfg.Durable = true;
-                });
+			cfg.ReceiveEndpoint(queueName, e =>
+			{
+				e.UseConcurrencyLimit(1);
+				e.StateMachineSaga(sagaStateMachine, sagaRepository);
+			});
 
-            return serviceCollection;
-        }
-
-        public static IServiceProvider RegisterConsumerWithRetry<TConsumer, TContract>(this IServiceProvider serviceProvider, int retryCount, int intervalMin, int concurrencyLimit = 0)
-	        where TConsumer : class, IConsumer where TContract : class
-        {
-	        var queueName = $"{typeof(TConsumer).FullName}_{typeof(TContract)}";
-
-	        _host.ConnectReceiveEndpoint(queueName, cfg =>
-	        {
-		        cfg.Consumer<TConsumer>(serviceProvider.GetService<TConsumer>,x =>
-		        {
-			        if (_useQuartz)
-			        {
-				        x.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5),
-					        TimeSpan.FromMinutes(15), TimeSpan.FromMinutes(30), TimeSpan.FromMinutes(60),
-					        TimeSpan.FromMinutes(120), TimeSpan.FromMinutes(240)));
-			        }
-
-			        x.UseMessageRetry(configurator =>
-			        {
-				        configurator.Interval(retryCount, TimeSpan.FromMinutes(intervalMin));
-			        });
-
-			        if (concurrencyLimit > 0)
-			        {
-				        x.UseConcurrencyLimit(concurrencyLimit);
-				        x.UseConcurrentMessageLimit(concurrencyLimit);
-			        }
-			        else
-			        {
-				        x.Message<TContract>(m => m.UsePartitioner(1, context => context.MessageId.Value));
-			        }
-		        });
-
-		        cfg.AutoDelete = false;
-		        cfg.Durable = true;
-	        });
-
-	        return serviceProvider;
-        }
-
-		public static IServiceCollection RegisterConsumer<TConsumer, TContract>(this IServiceCollection serviceCollection)
-            where TConsumer : class, IConsumer, new()
-        {
-            var queueName = $"{typeof(TConsumer).FullName}_{typeof(TContract)}";
-
-            _host.ConnectReceiveEndpoint(queueName, cfg =>
-            {
-                cfg.Consumer<TConsumer>();
-                cfg.AutoDelete = false;
-                cfg.Durable = true;
-            });
-
-            return serviceCollection;
-        }
-
+			return cfg;
+		}
 
 		public static IServiceCollection RegisterDataBusPublisher(this IServiceCollection serviceCollection)
 		{
@@ -149,22 +109,6 @@ namespace NetCoreDataBus
 		public static IServiceCollection RegisterDataBusPublisherStub(this IServiceCollection serviceCollection)
 		{
 			serviceCollection.AddSingleton<IBusPublisher, BusPublisherStub>();
-
-			return serviceCollection;
-		}
-
-		public static IServiceCollection RegisterSaga<TStateMachineInstance>(this IServiceCollection serviceCollection,
-			MassTransitStateMachine<TStateMachineInstance> sagaStateMachine,
-			EntityFrameworkSagaRepository<TStateMachineInstance> sagaRepository) 			
-			where TStateMachineInstance : class, SagaStateMachineInstance, new()
-		{
-			var queueName = $"{typeof(TStateMachineInstance).FullName}";
-
-			_host.ConnectReceiveEndpoint(queueName, cfg =>
-			{
-				cfg.PrefetchCount = 1;
-				cfg.StateMachineSaga(sagaStateMachine, sagaRepository);
-			});
 
 			return serviceCollection;
 		}
