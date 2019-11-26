fff5562f817ae9559e7b975e3b17f688153bced5
Сергей Лебедев <s.lebedev@magdv.com>
2019-11-26 13:57:31 +0700

    BusPublisher, добавлены методы SendScheduledMessageAsync, CancelScheduledMessageAsync

--- NetCoreDataBus/BusPublisherStub.cs
+++ NetCoreDataBus/BusPublisherStub.cs
@@ -1,13 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace NetCoreDataBus
 {
 	public class BusPublisherStub : IBusPublisher
 	{
+
 		public void Publish<T>(T message) where T : class { }
 		public Task PublishAsync<T>(T message) where T : class
 		{
 			return Task.CompletedTask;
+		}
+		public Task CancelScheduledMessageAsync(Guid tokenId)
+		{
+			return Task.CompletedTask;
+		}
+		public Task<Guid> SendScheduledMessageAsync<TMessage>(TMessage message, DateTime scheduledTime) where TMessage : class
+		{
+			return Task.FromResult(Guid.Empty);
 		}
 	}
 }
