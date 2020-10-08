c87faf76e288c089e1a96cc854cf02957557379a
a.caregorodcev <a.caregorodcev@magdv.com>
2020-10-08 11:46:47 +0700

    ключ

--- NetCoreCQRS/nuget_publish.cmd
+++ NetCoreCQRS/nuget_publish.cmd
@@ -1,4 +1,4 @@
 dotnet pack -c Release
-dotnet nuget push bin/Release/NetCoreCQRS.3.0.1.nupkg -k oy2ftcuea4qichnsk2yawfx4hkdagzienbq2yp2j3sdwsy -s https://api.nuget.org/v3/index.json
+dotnet nuget push bin/Release/NetCoreCQRS.3.0.1.nupkg -k oy2glaf2nvi7eqoybrsqfn7ynml5zjnejjtojznxzd6zrq -s https://api.nuget.org/v3/index.json
 
 pause
